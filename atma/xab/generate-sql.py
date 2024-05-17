0d3682ade7a88a78e59d742d78304ef41f09e710
irfanzainudin <m.irfan.zain@gmail.com>
2024-05-17 22:16:28 +1000

    trying a new approach to quicken the data cleaning

---
+++ atma/xab/generate-sql.py
@@ -0,0 +1,15 @@
+count = 110974
+query = f"INSERT INTO pantun (id, bayang1, bayang2, maksud1, maksud2, jenis, sumber) VALUES ({count},"
+with open("xab.txt", "r") as f:
+    with open("add-1.sql", "a") as a:
+        for line in f.readlines():
+            line = line.strip()
+            if line == "===":
+                count = count + 1
+                query = query + \
+                    f" 4, 6);\nINSERT INTO pantun (id, bayang1, bayang2, maksud1, maksud2, jenis, sumber) VALUES ({count},"
+            else:
+                line = "\"" + line
+                query = query + line + "\","
+        query = query + "4, 6);"
+        a.write(query)
